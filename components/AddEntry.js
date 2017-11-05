df823a46ae306e0119615f62a634c771f5d7bf9b
Radoslav Demirov <rdemirov@gmail.com>
2017-11-05 21:46:33 +0200

    feat : Add async storage

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 import {Ionicons} from '@expo/vector-icons'
 import TextButton from './TextButton'
+import {submitEntry,removeEntry} from '../utils/api'
 
 function SubmitBtn({ onPress }) {
     return (
@@ -72,8 +73,7 @@ class AddEntry extends Component {
 
         //Update redux
         //Navigate to home 
-
-        //Save to db
+        submitEntry({key,entry})
         //Clear local notifiactions
     }
 
@@ -81,7 +81,7 @@ class AddEntry extends Component {
         const key=timeToString();
         //Update redux
         //route to home
-        //update db
+       removeEntry({key})
     }
 
     render() {
